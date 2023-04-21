a9441367f15641a376287da45a13cf06a340cdb5
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-04-21 16:02:47 -0700

    missing tests added

--- src/maldi_tools/__init__.py
+++ src/maldi_tools/__init__.py
@@ -1,4 +1,9 @@
-"""Early Alpha of the Angelo Lab's Maldi Analysis Pipeline.
+"""Pre-Alpha of the Angelo Lab's Maldi Analysis Pipeline.
 
 Currently consists of one notebook, basic extraction and filtering and, minimal plotting functionality.
 """
+
+import matplotlib
+
+# Tk is a pain, don't use it.
+matplotlib.use("Agg")
